4540ebf3775a8eb7612b702c99e44dbcd0dd3534
Luka Mugosa <lukamugosa1945@gmail.com>
2021-06-02 22:01:14 +0200

    finishes module 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/question.dart';
+import 'package:flutter_complete_guide/quiz.dart';
+import 'package:flutter_complete_guide/result.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    // TODO: implement createState
     return _MyAppState();
   }
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-
-  var _questions = [
-    'What\'s your favorite color?',
-    'What\'s your favorite animal?'
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 7},
+        {'text': 'White', 'score': 8},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 5},
+        {'text': 'Elephant', 'score': 7},
+        {'text': 'Lion', 'score': 8},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'max', 'score': 1},
+      ],
+    },
   ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
     setState(() {
-      _questionIndex =
-          _questionIndex == _questions.length - 1 ? 0 : ++_questionIndex;
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+    this._totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    print('Odgovor odabran!');
+    if (_questionIndex < _questions.length) {
+      print('We have more _questions!');
+    } else {
+      print('No more _questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // _questions = []; // does not work if _questions is a const
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Moja prva aplikacija'),
+          title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(_questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Odgovor 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Odgovor 2'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Odgovor 3'),
-              onPressed: _answerQuestion,
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
